d19335cda5f53d9b0ccbc594f5bab4f583044409
161250102 <161250102@smail.nju.edu.cn>
2017-11-01 12:36:44 +0800

    修改Panel接口：删除Init方法

--- Client/src/presentation/PanelInterface.java
+++ Client/src/presentation/PanelInterface.java
@@ -13,9 +13,6 @@ public interface PanelInterface {
 	 */
 	public boolean close();
 	
-	//根据主窗口传递的VO对象初始化面板上的数据，注意与构造函数区别开
-	public void init(UserVO user);
-	
 	//返回所持有的JPanel对象的引用
 	public JPanel getPanel();
 	
