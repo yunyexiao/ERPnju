45377d2b3bbcd5f5806c2e22edcf4ce503f9181e
161250102 <161250102@smail.nju.edu.cn>
2017-11-01 12:41:51 +0800

    新增接口并添加注释

--- Client/src/blservice/UserBLService.java
+++ Client/src/blservice/UserBLService.java
@@ -1,20 +1,20 @@
 package blservice;
 
-import vo.CustomerVO;
+import vo.UserVO;
 
-public interface CustomerBLService extends DataBLService {
+public interface UserBLService extends DataBLService {
 
 	/**
 	 * 增加一条客户信息的记录
 	 * @param customer 客户VO
 	 * @return 是否成功添加
 	 */
-	public boolean add(CustomerVO customer);
+	public boolean add(UserVO user);
 	/**
 	 * 更改一条客户信息
 	 * @param customer 客户VO
 	 * @return 编号已存在并且成功更改返回true
 	 */
-	public boolean change(CustomerVO customer);
+	public boolean change(UserVO user);
 	
 }
