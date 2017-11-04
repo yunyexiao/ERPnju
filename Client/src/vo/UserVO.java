abcef9e4d4430db79e439ffa7cec2b4d97d7b227
161250102 <161250102@smail.nju.edu.cn>
2017-11-04 11:40:07 +0800

    增加了user的个人信息

--- Client/src/vo/UserVO.java
+++ Client/src/vo/UserVO.java
@@ -2,13 +2,21 @@ package vo;
 
 public class UserVO {
 	private String name;
+	private String id;
+	private String sex;
+	private String telNumber;
+	private int age;
 	//private String key;
 	private UserType type;
 	
-	public UserVO(String name, UserType type) {
+	public UserVO(String name, UserType type, String id, String sex, String telNumber, int age) {
 		this.name = name;
 		//this.key = key;
 		this.type = type;
+		this.id = id;
+		this.sex = sex;
+		this.telNumber = telNumber;
+		this.age = age;
 	}
 	
 	public UserType getType() {
