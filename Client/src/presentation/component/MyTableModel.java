5f52dd7670507562b86784acb995b31b1b700a4a
161250102 <161250102@smail.nju.edu.cn>
2017-11-21 13:59:52 +0800

    表格增加获取一行数据的方法

--- Client/src/presentation/component/MyTableModel.java
+++ Client/src/presentation/component/MyTableModel.java
@@ -3,7 +3,11 @@ package presentation.component;
 import java.util.Arrays;
 
 import javax.swing.table.DefaultTableModel;
-
+/**
+ * 继承DefaultTableModel的自定义类，用于实现不可修改的功能
+ * @author user
+ *
+ */
 public class MyTableModel extends DefaultTableModel {
 
 	//private int[] rows = {};
@@ -12,7 +16,10 @@ public class MyTableModel extends DefaultTableModel {
 	public MyTableModel(Object[][] data, Object[] columnNames) {
 		super(data, columnNames);
 	}
-	
+	/**
+	 * 设置可以编辑的列
+	 * @param columns 列号数组
+	 */
 	public void setEditable(int[] columns) {
 		this.columns = columns;
 	}
@@ -22,5 +29,17 @@ public class MyTableModel extends DefaultTableModel {
 			return true;
 		}
 		else return false;
+	}
+	/**
+	 * 获得某一行的数据(String数组形式)
+	 * @param n 行号
+	 * @return 某一行的数据
+	 */
+	public String[] getValueAtRow(int n) {
+		String[] data = new String[this.getColumnCount()];
+		for (int i = 0; i < data.length; i++) {
+			data[i] = (String) this.getValueAt(n, i);
+		}
+		return data;
 	}
 }
