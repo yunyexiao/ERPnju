341dc4ccc823b3e24ddd76987895ec30fde47291
161250102 <161250102@smail.nju.edu.cn>
2017-11-24 11:50:54 +0800

    实现接口的桩做出相应修改

--- Client/src/businesslogic/LogBL_stub.java
+++ Client/src/businesslogic/LogBL_stub.java
@@ -1,26 +1,25 @@
 package businesslogic;
 
-import javax.swing.table.DefaultTableModel;
-
 import blservice.LogBLService;
+import presentation.component.MyTableModel;
 
 public class LogBL_stub implements LogBLService {
 
 	@Override
-	public DefaultTableModel getLogInfo() {
+	public MyTableModel getLogInfo() {
 		String[] attributes={"操作时间","操作员","操作类型","详情"};
 		String[][] info={{"2017-11-06 12:48:32","他","新建进货单","…………"},
 				{"2017-11-06 12:32:32","他","增加商品","…………"}};
 		System.out.println("得到操作记录");
-		return new DefaultTableModel(info, attributes);
+		return new MyTableModel(info, attributes);
 	}
 
 	@Override
-	public DefaultTableModel searchByTime(String startTime, String endTime) {
+	public MyTableModel searchByTime(String startTime, String endTime) {
 		String[] attributes={"操作时间","操作员","操作类型","详情"};
 		String[][] info={{"2017-11-06 12:48:32","他","新建进货单","…………"}};
 		System.out.println("得到操作记录");
-		return new DefaultTableModel(info, attributes);
+		return new MyTableModel(info, attributes);
 	}
 
 }
