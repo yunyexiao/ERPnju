6c0d15b281bebabd5a610c30ef440599590d6475
161250102 <161250102@smail.nju.edu.cn>
2017-11-24 11:50:18 +0800

    修改接口返回类型为MyTableModel

--- Client/src/blservice/LogBLService.java
+++ Client/src/blservice/LogBLService.java
@@ -1,6 +1,6 @@
 package blservice;
 
-import javax.swing.table.DefaultTableModel;
+import presentation.component.MyTableModel;
 
 public interface LogBLService {
 
@@ -8,13 +8,13 @@ public interface LogBLService {
 	 * 得到所有操作记录
 	 * @return 直接用于表格显示的TableModel
 	 */
-	public DefaultTableModel getLogInfo();
+	public MyTableModel getLogInfo();
 	/**
 	 * 根据时间区间搜索操作记录
 	 * @param startTime 起始时间
 	 * @param endTime 结束时间
 	 * @return 直接用于表格显示的TableModel
 	 */
-	public DefaultTableModel searchByTime(String startTime, String endTime);
+	public MyTableModel searchByTime(String startTime, String endTime);
 	
 }
