857f8e558c43aa9bb656f0724a9cacf8bdadddb0
161250102 <161250102@smail.nju.edu.cn>
2017-11-25 20:48:47 +0800

    搜索条件选择使用对话框进行

--- Client/src/presentation/logui/LogPanel.java
+++ Client/src/presentation/logui/LogPanel.java
@@ -16,32 +16,36 @@ import presentation.component.CloseListener;
 import presentation.component.TopButtonPanel;
 import presentation.main.MainWindow;
 
+/**
+ * 查看日志使用的Panel
+ * @author 钱美缘
+ *
+ */
 public class LogPanel implements PanelInterface {
 	
-	private static double[][] size = {{TableLayout.FILL},{0.1,TableLayout.FILL}};
-	private JPanel panel = new JPanel(new TableLayout(size));
+	private JPanel panel;
 	private LogBLService logBL = new LogBL_stub();
 
 	public LogPanel(MainWindow mw) {
-		TopButtonPanel buttonPanel = new TopButtonPanel();
+		double[][] size = {{TableLayout.FILL},{0.1,TableLayout.FILL}};
+		panel = new JPanel(new TableLayout(size));
 		
 		JTable table = new JTable(logBL.getLogInfo());
 		JScrollPane scrollPane = new JScrollPane(table);
+		panel.add(scrollPane, "0,1");
 		
 		class SearchListener implements ActionListener{
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String[] re = buttonPanel.getCalender();
-				table.setModel(logBL.searchByTime(re[0], re[1]));
+				String[] input = new SearchWindow().getInput();
+				table.setModel(logBL.searchByTime(input[0], input[1]));
 			}
 		}
-		buttonPanel.addCalendar("起始日期");
-		buttonPanel.addCalendar("结束日期");
+		
+		TopButtonPanel buttonPanel = new TopButtonPanel();
 		buttonPanel.addButton("查询", new ImageIcon("resource/SearchData.png"), new SearchListener());
 		buttonPanel.addButton("关闭", new ImageIcon("resource/Close.png"), new CloseListener(mw));
-		
 		panel.add(buttonPanel.getPanel(), "0,0");
-		panel.add(scrollPane, "0,1");
 	}
 	
 	@Override
