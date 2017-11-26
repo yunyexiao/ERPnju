b9cbf2a8d002acab2f73ccc4ccc6b2dfa6a55cb3
161250102 <161250102@smail.nju.edu.cn>
2017-11-26 17:30:22 +0800

    不再使用DataRemoteObject

--- Server/src/rmi/RemoteHelper.java
+++ Server/src/rmi/RemoteHelper.java
@@ -10,33 +10,21 @@ import data.LoginData;
 public class RemoteHelper {
 
 	public RemoteHelper() {
-		//bind("data.LoginData");
 		try {
 			bind(new LoginData());
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		}
 	}
-	/*
-	private boolean bind(String className) {
-		try {
-			Class<?> c = Class.forName(className);
-			Object t = c.newInstance();
-			LocateRegistry.createRegistry(8080);
-			System.out.println(c.getInterfaces()[0].getName());
-			Naming.bind("rmi://localhost:8080/" + c.getInterfaces()[0].getName(), (Remote) t);
-			return true;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-	*/
+	/**
+	 * 注册一个远程对象
+	 * @param remote 需要绑定的接口
+	 */
 	private void bind(Remote remote) {
 		try {
 			LocateRegistry.createRegistry(8887);
-			System.out.println(remote.getClass().getName());
-			Naming.bind("rmi://localhost:8887/" + remote.getClass().getName(), remote);
+			System.out.println(remote.getClass().getInterfaces()[0].getName());
+			Naming.bind("rmi://localhost:8887/" + remote.getClass().getInterfaces()[0].getName(), remote);
 		} catch (Exception e) {
 			System.out.println("绑定" + remote.getClass().getName() + "出现问题");
 		}
