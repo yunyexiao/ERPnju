5affc117e57374575698de7a76407becf6ec3e3c
161250102 <161250102@smail.nju.edu.cn>
2017-11-26 17:39:35 +0800

    更新PO类

--- Server/src/po/CustomerPO.java
+++ Server/src/po/CustomerPO.java
@@ -15,7 +15,7 @@ public class CustomerPO implements Serializable {
      */
     private static final long serialVersionUID = -6850102650969420098L;
     
-    private String id, name, telNumber, address, mail;
+    private String id, name, telNumber, address, mail,code;
     /** 默认业务员 */
     private String salesman;
     /** 客户的级别，共5级 */
@@ -32,13 +32,14 @@ public class CustomerPO implements Serializable {
     public CustomerPO() {}
     
     public CustomerPO(String id, String name, String telNumber, String address
-        , String mail, String salesman, int rank, int type, double recRange
+        , String mail, String code,String salesman, int rank, int type, double recRange
         , double receivable, double payment) {
         this.setId(id);
         this.setName(name);
         this.setTelNumber(telNumber);
         this.setAddress(address);
         this.setMail(mail);
+   
         this.setSalesman(salesman);
         this.setRank(rank);
         this.setType(type);
@@ -85,6 +86,14 @@ public class CustomerPO implements Serializable {
 
     public void setMail(String mail) {
         this.mail = mail;
+    }
+    
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
     }
 
     public String getSalesman() {
