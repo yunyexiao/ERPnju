bf0e790787facaa039b76d2ce5227c6da34e317a
161250102 <161250102@smail.nju.edu.cn>
2017-11-26 09:53:56 +0800

    更改构造方法，新增toPO的转换

--- Client/src/vo/UserVO.java
+++ Client/src/vo/UserVO.java
@@ -1,25 +1,28 @@
 package vo;
 
+import po.UserPO;
+
 public class UserVO {
 	private String name;
 	private String id;
 	private String sex;
 	private String telNumber;
 	private int age;
-	//private String key;
+	private String key;
 	private UserType type;
 	/**
 	 * 构造函数……
 	 * @param name 姓名
+	 * @param key 密码
 	 * @param type 用户类别
 	 * @param id 编号
 	 * @param sex 性别
 	 * @param telNumber 电话
 	 * @param age 年龄
 	 */
-	public UserVO(String name, UserType type, String id, String sex, String telNumber, int age) {
+	public UserVO(String name, String key, UserType type, String id, String sex, String telNumber, int age) {
 		this.name = name;
-		//this.key = key;
+		this.key = key;
 		this.type = type;
 		this.id = id;
 		this.sex = sex;
@@ -33,5 +36,26 @@ public class UserVO {
 	
 	public String getName() {
 		return this.name;
+	}
+	
+	public String getId() {
+		return this.id;
+	}
+	
+	public String getSex() {
+		return this.sex;
+	}
+	
+	public String getTelNumber() {
+		return this.telNumber;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public UserPO toPO() {
+		UserPO userPO = new UserPO(id, name, " ", sex, telNumber, type.getNum(), age);
+		return userPO;
 	}
 }
