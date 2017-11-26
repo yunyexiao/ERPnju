a169aed6dad9c2334d8d53f5f3ae7a143a237510
161250102 <161250102@smail.nju.edu.cn>
2017-11-26 17:26:12 +0800

    启动时不再使用RemoteHelper

--- Client/src/Start.java
+++ Client/src/Start.java
@@ -1,35 +1,7 @@
-import java.net.MalformedURLException;
-import java.rmi.Naming;
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
-
 import presentation.LoginWindow;
-import rmi.RemoteHelper;
 
 public class Start {
-	private RemoteHelper remoteHelper;
-
-	private void linkToServer() {
-		try {
-			remoteHelper = RemoteHelper.getInstance();
-			remoteHelper.setRemote(Naming.lookup("rmi://172.25.181.218:8887/DataRemoteObject"));
-			System.out.println("linked");
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		} catch (NotBoundException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private Start() {
-		linkToServer();
-		new LoginWindow();
-	}
-	
 	public static void main(String[] args) {
-		//new Start();
 		new LoginWindow();
 	}
 
