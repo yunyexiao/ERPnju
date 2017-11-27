1f99263b533d498791ea46db7ec32ea6f4d17c81
161250102 <161250102@smail.nju.edu.cn>
2017-11-27 10:12:38 +0800

    增加桩

--- Client/src/bl_stub/CategoryBL_stub.java
+++ Client/src/bl_stub/CategoryBL_stub.java
@@ -10,27 +10,40 @@ public class CategoryBL_stub implements CategoryBLService {
 
 	@Override
 	public DefaultTreeModel getModel() {
-        DefaultMutableTreeNode root = new DefaultMutableTreeNode("商品类别");
-        root.add(new DefaultMutableTreeNode("蓝灯"));
-        root.add(new DefaultMutableTreeNode("交通信号灯"));
-        root.add(new DefaultMutableTreeNode("阿拉丁神灯"));
-        DefaultTreeModel treeModel = new DefaultTreeModel(root);
+		CategoryVO rootCategory = new CategoryVO("", "", "001-20171127-00001", "商品分类"),
+	               category1 = new CategoryVO("001-20171127-00001", "abc", "001-20171127-00002", "蓝灯"),
+	               category2 = new CategoryVO("001-20171127-00001", "def", "001-20171127-00003", "交通信号灯"),
+	               category3 = new CategoryVO("001-20171127-00001", "ghi", "001-20171127-00004", "阿拉丁神灯");
+
+		DefaultMutableTreeNode root = new DefaultMutableTreeNode(rootCategory);
+		root.add(new DefaultMutableTreeNode(category1));
+		root.add(new DefaultMutableTreeNode(category2));
+		root.add(new DefaultMutableTreeNode(category3));
+		DefaultTreeModel treeModel = new DefaultTreeModel(root);
 		return treeModel;
 	}
 
 	@Override
 	public boolean add(CategoryVO category) {
+		System.out.println("category added: " + category.getId());
 		return true;
 	}
 
 	@Override
-	public boolean delete(CategoryVO category) {
+	public boolean delete(String id) {
+		System.out.println("category deleted: " + id);
 		return true;
 	}
 
 	@Override
 	public boolean change(CategoryVO category) {
+		System.out.println("category changed: " + category.getId());
 		return true;
 	}
 
+	@Override
+    public String getNewId() {
+        return "001-20171127-002";
+    }
+	
 }
