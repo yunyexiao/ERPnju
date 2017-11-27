cc336a85ca34f25043600886f7dc0c3628852b3f
161250102 <161250102@smail.nju.edu.cn>
2017-11-27 10:28:22 +0800

    数据层的桩

---
+++ Client/src/ds_stub/CategoryDs_stub.java
@@ -0,0 +1,55 @@
+package ds_stub;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import dataservice.CategoryDataService;
+import po.CategoryPO;
+
+
+public class CategoryDs_stub implements CategoryDataService {
+
+    public CategoryDs_stub() {}
+
+    @Override
+    public String getNewId() throws RemoteException {
+        return "001-20171127-12345";
+    }
+
+    @Override
+    public CategoryPO findById(String id) throws RemoteException {
+        System.out.println("category found in database: " + id);
+        return new CategoryPO("001-201771127-00001", "some", "001-20171127-00002", "solar");
+    }
+
+    @Override
+    public boolean add(CategoryPO category) throws RemoteException {
+        System.out.println("category added in database: " + category.getId());
+        return true;
+    }
+
+    @Override
+    public boolean delete(String id) throws RemoteException {
+        System.out.println("category deleted in database: " + id);
+        return true;
+    }
+
+    @Override
+    public boolean update(CategoryPO category) throws RemoteException {
+        System.out.println("category updated in database: " + category.getId());
+        return true;
+    }
+
+    @Override
+    public ArrayList<CategoryPO> getAllCategory() throws RemoteException {
+        System.out.println("all categories in database returned");
+        ArrayList<CategoryPO> result = new ArrayList<>();
+        result.add(new CategoryPO("001-20171127-00000", "category-root", "", ""));
+        result.add(new CategoryPO("001-20171127-00001", "some", "001-20171127-00000", "category-root"));
+        result.add(new CategoryPO("001-20171127-00003", "same", "001-20171127-00000", "category-root"));
+        result.add(new CategoryPO("001-20171127-00002", "come", "001-20171127-00001", "some"));
+        result.add(new CategoryPO("001-20171127-00004", "sane", "001-20171127-00003", "same"));
+        return result;
+    }
+
+}
---
+++ Client/src/ds_stub/CommodityDs_stub.java
@@ -0,0 +1,55 @@
+package ds_stub;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import dataservice.CommodityDataService;
+import po.CommodityPO;
+
+
+public class CommodityDs_stub implements CommodityDataService {
+    
+    public CommodityDs_stub() {}
+
+    @Override
+    public String getNewId() throws RemoteException {
+        return "002-20171127-12345";
+    }
+
+    @Override
+    public CommodityPO findById(String id) throws RemoteException {
+        System.out.println("commodity found in database: " + id);
+        return new CommodityPO("002-20171127-00001", "交通信号灯", "TBD", "A", "特殊用途灯具", "001-20171127-00021"
+            , 2000L, 200L, 120.0, 340.0, 120.0, 320.0);
+    }
+
+    @Override
+    public boolean add(CommodityPO commodity) throws RemoteException {
+        System.out.println("commodity added in database: " + commodity.getId());
+        return true;
+    }
+
+    @Override
+    public boolean delete(String id) throws RemoteException {
+        System.out.println("commodity deleted in database: " + id);
+        return true;
+    }
+
+    @Override
+    public boolean update(CommodityPO commodity) throws RemoteException {
+        System.out.println("commodity updated in database: " + commodity.getId());
+        return true;
+    }
+
+    @Override
+    public ArrayList<CommodityPO> getAllCommodity() throws RemoteException {
+        System.out.println("all commodities in database returned");
+        ArrayList<CommodityPO> result = new ArrayList<>();
+        result.add(new CommodityPO("002-20171127-00001", "交通信号灯", "TBD", "A", "特殊用途灯具", "001-20171127-00021"
+            , 2000L, 200L, 120.0, 340.0, 120.0, 320.0));
+        result.add(new CommodityPO("002-20171127-00002", "de-light", "TDD", "A", "inspectional", "001-20171127-00539"
+            , 1000L, 100L, 200.0, 350.0, 200.0, 380.0));
+        return result;
+    }
+
+}
