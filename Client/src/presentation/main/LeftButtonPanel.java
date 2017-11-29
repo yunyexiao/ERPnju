034efeadce1addd3a72b0d3c37c6a744ee27a2d9
161250102 <161250102@smail.nju.edu.cn>
2017-11-29 11:07:06 +0800

    使用函数式编程语句替换Listener

--- Client/src/presentation/main/LeftButtonPanel.java
+++ Client/src/presentation/main/LeftButtonPanel.java
@@ -74,7 +74,7 @@ class LeftButtonPanel extends JPanel{
 		// 修改按钮处------------------------------
 		if (type == UserType.KEEPER) {
 			addButton("商品分类管理", e -> mw.changePanel(new CategoryDataPanel(new CategoryBL_stub(), closeListener)));
-			addButton("商品管理", new Listener(mw, new CommodityDataPanel(new CommodityBL_stub(), closeListener)));
+			addButton("商品管理", e -> mw.changePanel(new CommodityDataPanel(new CommodityBL_stub(), closeListener)));
 			addButton("库存查看", new Listener_stub());
 			addButton("库存盘点", new Listener_stub());
 			addButton("报溢/报损", new Listener_stub());
@@ -82,36 +82,36 @@ class LeftButtonPanel extends JPanel{
 			
 		}
 		else if (type == UserType.SALESMAN) {
-			addButton("客户管理", new Listener(mw, new MockDataPanel(new CustomerBL_stub(), closeListener)));
-			addButton("制定进货单", new Listener(mw, new BillPanel(mw, BillType.PURCHASE, true)));
+			addButton("客户管理", e -> mw.changePanel(new MockDataPanel(new CustomerBL_stub(), closeListener)));
+			addButton("制定进货单", e -> mw.changePanel(new BillPanel(mw, BillType.PURCHASE, true)));
 			addButton("制定进货退货单", new Listener_stub());
-			addButton("制定销售单", new Listener(mw, new SaleBillPanel(mainWindow.getUser(), closeListener)));
+			addButton("制定销售单", e -> mw.changePanel(new SaleBillPanel(mainWindow.getUser(), closeListener)));
 			addButton("制定销售退货单", new Listener_stub());
 			addButton("退出", new CloseListener());
 		}
 		else if (type == UserType.ACCOUNTANT) {
-			addButton("账户管理", new Listener(mw, new MockDataPanel(new AccountBL_stub(), closeListener)));
+			addButton("账户管理", e -> mw.changePanel(new MockDataPanel(new AccountBL_stub(), closeListener)));
 			addButton("制定收付款单", new Listener_stub());
 			addButton("制定现金费用单", new Listener_stub());
 			addButton("查看销售明细表", new Listener_stub());
 			addButton("查看经营状况表", new Listener_stub());
 			addButton("查看经营历程表", new Listener_stub());
 			addButton("期初建账", new Listener_stub());
-			addButton("查看日志", new Listener(mw, new LogPanel(mw)));		
+			addButton("查看日志", e -> mw.changePanel(new LogPanel(mw)));		
 			addButton("退出", new CloseListener());
 		}
 		else if (type == UserType.GM) {
-			addButton("审批单据", new Listener(mw, new BillExaminePanel(mw)));
+			addButton("审批单据", e -> mw.changePanel(new BillExaminePanel(mw)));
 			addButton("制定促销策略", new Listener_stub());
 			addButton("查看销售明细表", new Listener_stub());
 			addButton("查看经营状况表", new Listener_stub());
 			addButton("查看经营历程表", new Listener_stub());
-			addButton("查看日志", new Listener(mw, new LogPanel(mw)));		
+			addButton("查看日志", e -> mw.changePanel(new LogPanel(mw)));		
 			addButton("退出", new CloseListener());
 		}
 		else if (type == UserType.ADMIN) {
-			addButton("用户管理", new Listener(mw, new UserDataPanel(new UserBL_stub(), closeListener)));
-			addButton("查看日志", new Listener(mw, new LogPanel(mw)));		
+			addButton("用户管理", e -> mw.changePanel(new UserDataPanel(new UserBL_stub(), closeListener)));
+			addButton("查看日志", e -> mw.changePanel(new LogPanel(mw)));		
 			addButton("退出", new CloseListener());
 		}
 		//-----------------------------------------
