c58d457afe5cefefc9b022fc9049b29837e9ff6c
UsagiHao <506887402@qq.com>
2017-12-01 19:13:31 +0800

    Merge branch 'develop' into '037develop'

--- Client/src/vo/AccountVO.java
+++ Client/src/vo/AccountVO.java
@@ -6,9 +6,9 @@ public class AccountVO {
 
 	private String name;
 	private String number;
-	private int money;
+	private double money;
 	
-	public AccountVO(String name, String number, int money) {
+	public AccountVO(String name, String number, double money) {
 		this.name = name;
 		this.number = number;
 		this.money = money;
@@ -16,7 +16,7 @@ public class AccountVO {
 	
 	public String getName() {return name;}
 	public String getNumber() {return number;}
-	public int getMoney() {return money;}
+	public double getMoney() {return money;}
 	
 	public AccountPO toPO() {
 		AccountPO accountPO = new AccountPO (number, name, money);
