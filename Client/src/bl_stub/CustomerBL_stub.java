b9aa1992fc03c6ad4c5480ef7b6ed0ba097f5257
UsagiHao <506887402@qq.com>
2017-12-01 19:51:21 +0800

    改变了桩中示例信息显示顺序

--- Client/src/bl_stub/CustomerBL_stub.java
+++ Client/src/bl_stub/CustomerBL_stub.java
@@ -20,9 +20,9 @@ public class CustomerBL_stub implements CustomerBLService {
 
 	@Override
 	public MyTableModel update() {
-		String[] attributes={"客户编号","姓名","分类","级别","应收额度","应收","应付","电话","邮编","电子邮箱","默认业务员"};
-		String[][] info={{"JHS-001","清流","进货商","LV5","4000","0","0","","","","Van"},
-				{"XSS-001","浊流","销售商","LV1","2000","100","400","","","","Van"}};
+		String[] attributes={"客户编号","姓名","分类","级别","电话","地址","邮编","电子邮箱","应收额度","应收","应付","默认业务员"};
+		String[][] info={{"JHS-001","清流","进货商","LV5","","deep dark ♂ fantasy","","","4000","0","0","Van"},
+				{"XSS-001","浊流","销售商","LV1","","幻想♂乡","","","2000","100","400","Van"}};
 		System.out.println("客户信息已成功更新");
 		return new MyTableModel(info, attributes);
 	}
