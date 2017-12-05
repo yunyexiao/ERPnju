929d8ca668fd29ea684c4990ddb5f80deaeade9b
UsagiHao <506887402@qq.com>
2017-12-05 10:47:43 +0800

    新增现金费用单数据层的桩

---
+++ Client/src/ds_stub/CashCostBillDs_stub.java
@@ -0,0 +1,30 @@
+package ds_stub;
+
+import dataservice.CashCostBillDataService;
+import po.billpo.CashCostBillPO;
+
+public class CashCostBillDs_stub implements CashCostBillDataService {
+
+	@Override
+	public boolean saveBill(CashCostBillPO bill) {
+        System.out.println("cashcost bill saved in database: " + bill.getId());
+		return true;
+	}
+
+	@Override
+	public boolean deleteBill(String billid) {
+        System.out.println("cashcost bill deleted in database: " + billid);
+		return true;
+	}
+
+	@Override
+	public String getNewId() {
+		return "00001";
+	}
+
+	@Override
+	public CashCostBillPO getBillById(String id) {
+		return null;
+	}
+
+}
