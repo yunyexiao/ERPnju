ee7df5e86f1b7aea9df6d1a5267819dc3aeb7de4
161250102 <161250102@smail.nju.edu.cn>
2017-12-06 14:35:52 +0800

    新增两个高权限用户

--- Client/src/ds_stub/UserDs_stub.java
+++ Client/src/ds_stub/UserDs_stub.java
@@ -14,10 +14,12 @@ public class UserDs_stub implements UserDataService {
 	public UserDs_stub() {
 		if (result.size() == 0) {
 			result.add(new UserPO("0001","他", "", "男", "11122233344", UserType.KEEPER.getNum(), 0, 91,true));
-			result.add(new UserPO("0002","香港记者", "", "男", "12345678900", UserType.SALESMAN.getNum(), 0, 55,true));
+			result.add(new UserPO("0002","香港记者", "", "女", "12345678900", UserType.SALESMAN.getNum(), 0, 55,true));
 			result.add(new UserPO("0003","长者", "", "男", "12345432112", UserType.ACCOUNTANT.getNum(), 0, 88,true));
 			result.add(new UserPO("0004","比利海灵顿", "", "男", "18851826666", UserType.GM.getNum(), 0, 31,true));
 			result.add(new UserPO("0005","梁逸峰", "", "男", "18851827777", UserType.ADMIN.getNum(), 0, 46,true));
+			result.add(new UserPO("0006","金坷垃", "", "男", "13245432112", UserType.ACCOUNTANT.getNum(), 1, 55,true));
+			result.add(new UserPO("0007","西方记者", "", "男", "12345600088", UserType.SALESMAN.getNum(), 1, 35,true));
 		}
 	}
 	@Override
