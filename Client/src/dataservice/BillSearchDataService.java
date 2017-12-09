7808481b7d5f56dd09ce51a1cc04635233d2e9d2
161250192 <161250192@mail.smal.nju.edu.cn>
2017-12-09 22:12:19 +0800

    数据层接口定义与桩简单完成

---
+++ Client/src/dataservice/BillSearchDataService.java
@@ -0,0 +1,41 @@
+package dataservice;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import po.billpo.CashCostBillPO;
+import po.billpo.ChangeBillPO;
+import po.billpo.PaymentBillPO;
+import po.billpo.PurchaseBillPO;
+import po.billpo.PurchaseReturnBillPO;
+import po.billpo.ReceiptBillPO;
+import po.billpo.SalesBillPO;
+import po.billpo.SalesReturnBillPO;
+
+public interface BillSearchDataService {
+
+    ArrayList<PurchaseBillPO> searchPurchaseBills(String fromDate, String toDate
+        , String customerId, String operatorId) throws RemoteException;
+
+    ArrayList<PurchaseReturnBillPO> searchPurchaseReturnBills(String fromDate, String toDate
+        , String customerId, String operatorId) throws RemoteException;
+
+    ArrayList<SalesBillPO> searchSalesBills(String fromDate, String toDate
+        , String customerId, String operatorId) throws RemoteException;
+
+    ArrayList<SalesReturnBillPO> searchSalesReturnBills(String fromDate, String toDate
+        , String customerId, String operatorId) throws RemoteException;
+
+    ArrayList<CashCostBillPO> searchCashCostBills(String fromDate, String toDate
+        , String customerId, String operatorId) throws RemoteException;
+
+    ArrayList<ChangeBillPO> searchChangeBills(String fromDate, String toDate
+        , String store, String operatorId) throws RemoteException;
+
+    ArrayList<PaymentBillPO> searchPaymentBills(String fromDate, String toDate
+        , String customerId, String operatorId) throws RemoteException;
+
+    ArrayList<ReceiptBillPO> searchReceiptBills(String fromDate, String toDate
+        , String customerId, String operatorId) throws RemoteException;
+
+}
