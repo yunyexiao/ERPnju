cd477e7d62a2777122af52d812a8ea5657a8c060
161250102 <161250102@smail.nju.edu.cn>
2017-12-19 11:10:14 +0800

    新增进货销售人员单据的桩

--- Client/src/dataservice/BillDataService.java
+++ Client/src/dataservice/BillDataService.java
@@ -5,10 +5,22 @@ import java.util.ArrayList;
 import po.UserPO;
 import po.billpo.BillPO;
 import po.billpo.ChangeBillPO;
+import po.billpo.PurchaseBillPO;
+import po.billpo.PurchaseReturnBillPO;
+import po.billpo.SalesBillPO;
+import po.billpo.SalesReturnBillPO;
 
 public interface BillDataService {
 	
 	public ArrayList<BillPO> getBillList(UserPO user);
 	
 	public ChangeBillPO getChangeBill(String id);
+	
+	public PurchaseBillPO getPurchaseBill(String id);
+	
+	public PurchaseReturnBillPO getPurchaseReturnBill(String id);
+	
+	public SalesBillPO getSalesBill(String id);
+	
+	public SalesReturnBillPO getSalesReturnBill(String id);
 }
