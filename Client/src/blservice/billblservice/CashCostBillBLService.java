858c3854966a9abe71a8add1422a2ca3ccdbe654
161250102 <161250102@smail.nju.edu.cn>
2017-12-19 19:04:12 +0800

    单据制定接口中删除getById方法

--- Client/src/blservice/billblservice/CashCostBillBLService.java
+++ Client/src/blservice/billblservice/CashCostBillBLService.java
@@ -2,19 +2,12 @@ package blservice.billblservice;
 
 import vo.billvo.CashCostBillVO;
 
-public interface CashCostBillBLService extends BillBLService {
+public interface CashCostBillBLService extends BillCreateBLService {
     boolean updateBill(CashCostBillVO bill);
 	
 	/**
 	 * 将单据保存到数据库（和保存单据不van全一致）
 	 * @return 是否保存成功
 	 */
-	public boolean saveBill(CashCostBillVO bill);
-	/**
-	 * 这里会将PO转换成VO
-	 * @param id 完整的id标号
-	 * @return 现金费用单据的VO类
-	 */
-	public CashCostBillVO getBill(String id);
-
+	boolean saveBill(CashCostBillVO bill);
 }
