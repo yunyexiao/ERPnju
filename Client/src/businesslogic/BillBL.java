e9c01c9b2ecf4c07746c61db3a5b72f7a5f9d2b6
161250102 <161250102@smail.nju.edu.cn>
2017-12-19 19:36:18 +0800

    增加异常处理语句

--- Client/src/businesslogic/BillBL.java
+++ Client/src/businesslogic/BillBL.java
@@ -1,5 +1,6 @@
 package businesslogic;
 
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -27,63 +28,108 @@ public class BillBL implements BillBLService {
 	
 	@Override
 	public MyTableModel getBillTable(UserVO user) {
-		ArrayList<BillPO> list = billDs.getBillList(user.toPO());
-		String[] attributes = new String[]{"制定时间","单据编号","单据类型","状态"};
-		String[][] info = new String[list.size()][attributes.length];
-		
-		Collections.sort(list, new Comparator<BillPO>() {  
-            @Override  
-            public int compare(BillPO o1, BillPO o2) {
-            	int[] states = {BillPO.SAVED, BillPO.NOTPASS, BillPO.COMMITED, BillPO.PASS};
-            	if (Arrays.binarySearch(states, o1.getState()) == Arrays.binarySearch(states, o2.getState())) {
-            		if (o1.getDate().equals(o2.getDate())) {
-                		return Integer.parseInt(o1.getTime()) >= Integer.parseInt(o2.getTime()) ? 1 : -1;
-                	} else return Integer.parseInt(o1.getDate()) > Integer.parseInt(o2.getDate()) ? 1 : -1;
-            	} else return Arrays.binarySearch(states, o1.getState()) > Arrays.binarySearch(states, o2.getState()) ? 1 : -1 ;
-            }  
-        });
-		
-		for (int i = 0; i < list.size(); i++) {
-			BillPO bill = list.get(i);
-			info[i][0] = bill.getDate() + "  " + bill.getTime();
-			info[i][1] = bill.getAllId();
-			info[i][2] = BillTools.getBillName(bill);
-			info[i][3] = bill.getStateName();
+		try {
+			ArrayList<BillPO> list = billDs.getBillList(user.toPO());
+			String[] attributes = new String[]{"制定时间","单据编号","单据类型","状态"};
+			String[][] info = new String[list.size()][attributes.length];
+			
+			Collections.sort(list, new Comparator<BillPO>() {  
+	            @Override  
+	            public int compare(BillPO o1, BillPO o2) {
+	            	int[] states = {BillPO.SAVED, BillPO.NOTPASS, BillPO.COMMITED, BillPO.PASS};
+	            	if (Arrays.binarySearch(states, o1.getState()) == Arrays.binarySearch(states, o2.getState())) {
+	            		if (o1.getDate().equals(o2.getDate())) {
+	                		return Integer.parseInt(o1.getTime()) >= Integer.parseInt(o2.getTime()) ? 1 : -1;
+	                	} else return Integer.parseInt(o1.getDate()) > Integer.parseInt(o2.getDate()) ? 1 : -1;
+	            	} else return Arrays.binarySearch(states, o1.getState()) > Arrays.binarySearch(states, o2.getState()) ? 1 : -1 ;
+	            }  
+	        });
+			
+			for (int i = 0; i < list.size(); i++) {
+				BillPO bill = list.get(i);
+				info[i][0] = bill.getDate() + "  " + bill.getTime();
+				info[i][1] = bill.getAllId();
+				info[i][2] = BillTools.getBillName(bill);
+				info[i][3] = bill.getStateName();
+			}
+			
+			return new MyTableModel(info, attributes);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			return null;
 		}
-		
-		return new MyTableModel(info, attributes);
 	}
 
 	@Override
 	public ChangeBillVO getChangeBill(String id) {
-		return BillTools.toChangeBillVO(billDs.getChangeBill(id));
+		try {
+			return BillTools.toChangeBillVO(billDs.getChangeBill(id));
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
 	public PurchaseBillVO getPurchaseBill(String id) {
-		return BillTools.toPurchaseBillVO(billDs.getPurchaseBill(id));
+		try {
+			return BillTools.toPurchaseBillVO(billDs.getPurchaseBill(id));
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	public PurchaseReturnBillVO getPurchaseReturnBill(String id) {
-		return BillTools.toPurchaseReturnBillVO(billDs.getPurchaseReturnBill(id));
+		try {
+			return BillTools.toPurchaseReturnBillVO(billDs.getPurchaseReturnBill(id));
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
 	public SalesBillVO getSalesBill(String id) {
-		return BillTools.toSalesBillVO(billDs.getSalesBill(id));
+		try {
+			return BillTools.toSalesBillVO(billDs.getSalesBill(id));
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 	public SalesReturnBillVO getSalesReturnBill(String id) {
-		return BillTools.toSalesReturnBillVO(billDs.getSalesReturnBill(id), this);
+		try {
+			return BillTools.toSalesReturnBillVO(billDs.getSalesReturnBill(id), this);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
 	public CashCostBillVO getCashCostBill(String id) {
-		return BillTools.toCashCostBillVO(billDs.getCashCostBillPO(id));
+		try {
+			return BillTools.toCashCostBillVO(billDs.getCashCostBillPO(id));
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
 	public PaymentBillVO getPaymentBill(String id) {
-		return BillTools.toPaymentBillVO(billDs.getPaymentBillPO(id));
+		try {
+			return BillTools.toPaymentBillVO(billDs.getPaymentBillPO(id));
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 	
 	public ReceiptBillVO getReceiptBill(String id) {
-		return BillTools.toReceiptBillVO(billDs.getReceiptBillPO(id));
+		try {
+			return BillTools.toReceiptBillVO(billDs.getReceiptBillPO(id));
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
