5f3e0f06e55a11179370c00af2ff2d90c6760a1a
161250102 <161250102@smail.nju.edu.cn>
2017-12-21 19:43:09 +0800

    删去CustomerName属性

--- Client/src/vo/billvo/PurchaseBillVO.java
+++ Client/src/vo/billvo/PurchaseBillVO.java
@@ -5,17 +5,13 @@ import presentation.component.MyTableModel;
 
 public class PurchaseBillVO extends MarketBillVO {
     
-    public PurchaseBillVO(String date, String time, String id
-        , String operator, int state, String customerId,
-        String customerName, MyTableModel model
-        , String remark, double sum) {
-        super(date, time, id, operator, state, customerId
-            , customerName, model, remark, sum);
+    public PurchaseBillVO(String date, String time, String id, String operator
+    		, int state, String customerId, MyTableModel model, String remark, double sum) {
+        super(date, time, id, operator, state, customerId, model, remark, sum);
     }
     
     @Override
     protected String getPrefix() {
         return "JHD";
     }
-
 }
