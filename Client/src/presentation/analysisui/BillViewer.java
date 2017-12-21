d971d67a3e9121185962a8e6704473926a5a667b
161250102 <161250102@smail.nju.edu.cn>
2017-12-21 19:19:43 +0800

    单据Panel使用接口之后的BilViewer

--- Client/src/presentation/analysisui/BillViewer.java
+++ Client/src/presentation/analysisui/BillViewer.java
@@ -1,84 +1,57 @@
 package presentation.analysisui;
 
-import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import blservice.billblservice.BillOperationService;
 import businesslogic.BillOperationBL;
-import presentation.PanelInterface;
-import presentation.billui.BillPanel;
+import presentation.billui.BillPanelHelper;
 import presentation.billui.ChangeBillPanel;
-import vo.UserVO;
 import vo.billvo.BillVO;
 
 class BillViewer {
     
     private JDialog frame;
-    private PanelInterface panel;
-    private boolean editable;
+    private JPanel panel;
     
-    public BillViewer(Class<? extends BillPanel> panelType, 
-        Class<? extends BillVO> billType, UserVO user, BillVO bill) {
-        editable = bill.getState() == BillVO.SAVED;
-        frame = new JDialog();
+    public BillViewer(BillVO bill, boolean isCopy) {
+    	frame = new JDialog();
         frame.setModal(true);
         frame.setTitle("查看单据具体内容");
-        ActionListener closeListener = e -> frame.dispose();
-        try{
-            panel = panelType.getConstructor(UserVO.class, ActionListener.class, billType)
-                        .newInstance(user, closeListener, billType.cast(bill));
-            adjustTopPanel();
-            frame.setContentPane(panel.getPanel());
-            frame.setLocation(300, 100);
-            frame.setSize(800, 600);
-            frame.setVisible(true);
-        }catch(Exception e){
-            e.printStackTrace();
-            frame.dispose();
-        }
+        panel = (JPanel) BillPanelHelper.createInner(bill);
+        frame.setContentPane(panel);
+        frame.setLocation(300, 100);
+        frame.setSize(800, 600);
+        
+        frame.addWindowListener(new WindowAdapter() {
+        	public void windowClosing(WindowEvent event) {
+        		if (isCopy) {
+        			try{
+        	            BillVO bill;
+        	            if(panel instanceof ChangeBillPanel){
+        	                bill = ((ChangeBillPanel) panel).getBill();
+        	                bill.setState(BillVO.PASS);
+        	            } else {
+        	                bill = (BillVO)panel.getClass().getMethod("getBill", int.class).invoke(panel, BillVO.PASS);
+        	            }
+        	            if(bill == null) return;
+        	            BillOperationService billOperationBl = new BillOperationBL();
+        	            if(billOperationBl.copyBill(bill)){
+        	                JOptionPane.showMessageDialog(null, "红冲并复制成功");
+        	                frame.dispose();
+        	            } else {
+        	                JOptionPane.showMessageDialog(null, "红冲并复制失败，请重试");
+        	            }
+        	        }catch(Exception e){
+        	            e.printStackTrace();
+        	        }
+        		}
+        	}
+        });
+        frame.setVisible(true);
     }
-
-    private void adjustTopPanel(){
-        JPanel topPanel = (JPanel)panel.getPanel().getComponents()[0];
-        topPanel.remove(0);
-        topPanel.remove(0);
-        if(editable){
-            JButton okButton = (JButton)topPanel.getComponent(0);
-            okButton.removeActionListener(okButton.getActionListeners()[0]);
-            okButton.setText("完成并生效");
-            okButton.addActionListener(e -> copyBill());
-        } else {
-            topPanel.remove(0);
-        }
-    }
-    
-    /**
-     * 红冲并复制，直接生效，注意直接生效这一点是由{@code BillOperationService}决定的
-     */
-    private void copyBill(){
-        try{
-            BillVO bill;
-            if(panel instanceof ChangeBillPanel){
-                bill = ((ChangeBillPanel) panel).getBill();
-                bill.setState(BillVO.PASS);
-            } else {
-                bill = (BillVO)panel.getClass().getMethod("getBill", int.class).invoke(panel, BillVO.PASS);
-            }
-            if(bill == null) return;
-            BillOperationService billOperationBl = new BillOperationBL();
-            if(billOperationBl.copyBill(bill)){
-                JOptionPane.showMessageDialog(null, "红冲并复制成功");
-                frame.dispose();
-            } else {
-                JOptionPane.showMessageDialog(null, "红冲并复制失败，请重试");
-            }
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
-    
 }
